005ed4df399485815cc7d0ff2181c3229d18ef30
Wouter Damen <damen.wouter@gmail.com>
2026-04-02 17:03:39 +0200

    Add gradient animation

--- src/js/index.js
+++ src/js/index.js
@@ -15,4 +15,23 @@ window.addEventListener("load", () => {
   });
 
   document.querySelector("#landing").classList.add("in");
+  animateGradient();
 });
+
+const speed = 0.15;
+let offset = -50;
+
+function animateGradient() {
+  offset -= speed;
+
+  const x = offset % 100;
+  const t = -Math.abs(100 + 2 * x);
+  const s = 120 * Math.sin(t / 100);
+  const gradientY = 80 + s; // Loop the animation
+
+  document
+    .querySelector("#landing")
+    .setAttribute("style", `--gradient-pos:${gradientY}%`);
+
+  requestAnimationFrame(animateGradient);
+}
